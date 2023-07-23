352fccb52cd55f8f5407f64820da86c570973159
Mohamed Hany <mohamed.hany@enozom.com>
2023-07-24 01:27:31 +0300

    adding admin functionality for CRUD time

--- app/controllers/jogging_times_controller.rb
+++ app/controllers/jogging_times_controller.rb
@@ -1,11 +1,17 @@
 class JoggingTimesController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :require_user
+    before_action :set_user, only: [:index, :create]
     before_action :set_jogging_time, only: [:show, :update, :destroy]
-
+    before_action :authenticate_admin, only: [:index]
 
     def index
-        jogging_times = current_user.jogging_times
+        if @user
+            # Jogging times for a specific user (Admin access)
+            jogging_times = @user.jogging_times
+        else
+            jogging_times = current_user.jogging_times
+        end
         render json: jogging_times
     end
 
@@ -15,11 +21,12 @@ class JoggingTimesController < ApplicationController
 
     def create
         jogging_time = JoggingTime.new(jogging_time_params)
-        jogging_time.user_id = session[:user_id]
+        jogging_time.user_id = @user?@user.id : session[:user_id]
+        
         if jogging_time.save
-        render json: jogging_time, status: :created
+            render json: jogging_time, status: :created
         else
-        render json: { errors: jogging_time.errors.full_messages }, status: :bad_request
+            render json: { errors: jogging_time.errors.full_messages }, status: :bad_request
         end
     end
 
@@ -52,6 +59,18 @@ class JoggingTimesController < ApplicationController
         params.permit(:time, :distance)
     end 
 
+    def set_user
+        @user = User.find_by(id: params[:user_id]) if params[:user_id]
+        if  @user.nil? && params[:user_id]
+            render json: { error: 'wrong user id' }, status: :bad_request
+        end
+    end
 
+    def authenticate_admin
+        if current_user.role_id != 1 && @user
+          render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
+    end
+    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy' 
-  resources :jogging_times, except: [:new, :edit]
 
   resources :users, except: [:new, :edit]
+  resources :jogging_times, only: [:index, :create, :show, :update, :destroy]
+
+  resources :users, only: [] do
+    resources :jogging_times, only: [:index,:create], on: :member
+  end
+
 end
