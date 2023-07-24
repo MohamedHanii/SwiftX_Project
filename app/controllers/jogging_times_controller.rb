7127637fb14d5a9f45e1b2052d2f23273b7ddbec
Mohamed Hany <mohamed.hany@enozom.com>
2023-07-24 22:58:02 +0300

    adding filter from - to

--- app/controllers/jogging_times_controller.rb
+++ app/controllers/jogging_times_controller.rb
@@ -5,20 +5,34 @@ class JoggingTimesController < ApplicationController
     before_action :set_jogging_time, only: [:show, :update, :destroy]
     before_action :authenticate_admin, only: [:index]
 
+    #GET /jogging_time
+    #Get all jogging time for current user
+    #GET /user/:id/jogging_time
+    #Get all jogging time for user_id (admin access only)
     def index
+        from_date = params.fetch(:from, Date.new(1900, 1, 1)).to_time.beginning_of_day.strftime('%Y-%m-%d %H:%M:%S.%6N')
+        to_date = params.fetch(:to, Date.current).to_time.end_of_day.strftime('%Y-%m-%d %H:%M:%S.%6N')
+        
+        print to_date
         if @user
             # Jogging times for a specific user (Admin access)
-            jogging_times = @user.jogging_times
+            jogging_times = @user.jogging_times.where(created_at: from_date..to_date)
         else
-            jogging_times = current_user.jogging_times
+            jogging_times = current_user.jogging_times.where(created_at: from_date..to_date)
         end
         render json: jogging_times
     end
 
+    #GET /jogging_time/:joggging_time_id
+    #Get jogging time with id
     def show
         render json: @jogging_time
     end
 
+    #POST /jogging_time
+    #create new jogging time for current user
+    #POST /user/:id/jogging_time
+    #create new jogging time for user_id (admin access only)
     def create
         jogging_time = JoggingTime.new(jogging_time_params)
         jogging_time.user_id = @user?@user.id : session[:user_id]
@@ -30,7 +44,8 @@ class JoggingTimesController < ApplicationController
         end
     end
 
-
+    #PUT /jogging_time/:jogging_time_id
+    #Update jogging time with id
     def update
         if @jogging_time.update(jogging_time_params)
             render json: @jogging_time
@@ -39,7 +54,8 @@ class JoggingTimesController < ApplicationController
         end
     end
 
-
+    #DELETE /jogging_time/:jogging_time_id
+    #Delete jogging time with id
     def destroy
         @jogging_time.destroy
         render json: { message: 'Jogging time deleted successfully' }, status: :no_content
